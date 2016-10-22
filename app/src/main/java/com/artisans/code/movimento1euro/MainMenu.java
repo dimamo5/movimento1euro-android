1a56b40ab93c09361d5615e0609729b3131ec8d8
444Duarte <duartemr.pinto@gmail.com>
2016-10-22 15:12:56 +0100

    Resized nav user image. Added status bar color.

--- app/src/main/java/com/artisans/code/movimento1euro/MainMenu.java
+++ app/src/main/java/com/artisans/code/movimento1euro/MainMenu.java
@@ -1,8 +1,10 @@
 package com.artisans.code.movimento1euro;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -40,6 +42,10 @@ public class MainMenu extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.getWindow().setStatusBarColor(ContextCompat.getColor(this.getApplicationContext(),R.color.colorPrimaryDark));
+        }
     }
 
     @Override
@@ -80,7 +86,7 @@ public class MainMenu extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        /*if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
 
@@ -91,8 +97,7 @@ public class MainMenu extends AppCompatActivity
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
-
-        }
+        }*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
