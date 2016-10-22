ab36647fc98fc8f4a45bb695a390d70daadadbfc
444Duarte <duartemr.pinto@gmail.com>
2016-10-22 17:45:13 +0100

    News webview added

--- app/src/main/java/com/artisans/code/movimento1euro/menus/MainMenu.java
+++ app/src/main/java/com/artisans/code/movimento1euro/menus/MainMenu.java
@@ -1,10 +1,11 @@
-package com.artisans.code.movimento1euro;
+package com.artisans.code.movimento1euro.menus;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +15,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.artisans.code.movimento1euro.R;
 
 public class MainMenu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -44,7 +47,7 @@ public class MainMenu extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            this.getWindow().setStatusBarColor(ContextCompat.getColor(this.getApplicationContext(),R.color.colorPrimaryDark));
+            //this.getWindow().setStatusBarColor(ContextCompat.getColor(this.getApplicationContext(),R.color.colorPrimaryDark));
         }
     }
 
@@ -86,21 +89,20 @@ public class MainMenu extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        /*if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-        }*/
+        if (id == R.id.nav_news){
+            Uri uri = Uri.parse("http://www.google.com");
+            Intent intent = new Intent(this, WebViewActivity.class);
+            intent.putExtra("url", uri);
+            startActivity(intent);
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
---
+++ app/src/main/java/com/artisans/code/movimento1euro/menus/WebViewActivity.java
@@ -0,0 +1,57 @@
+package com.artisans.code.movimento1euro.menus;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.artisans.code.movimento1euro.R;
+
+/**
+ * Created by Duarte on 22/10/2016.
+ */
+
+public class WebViewActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_PROGRESS);
+        setContentView(R.layout.webview_layout);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.back_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        WebView webview = (WebView) findViewById(R.id.webview);
+        // Let's display the progress in the activity title bar, like the
+        // browser app does.
+
+        webview.getSettings().setJavaScriptEnabled(true);
+        final Activity activity = this;
+        webview.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int progress) {
+                // Activities and WebViews measure progress with different scales.
+                // The progress meter will automatically disappear when we reach 100%
+                activity.setProgress(progress * 1000);
+            }
+        });
+        webview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        webview.loadUrl(((Uri) getIntent().getExtras().get("url")).toString());
+    }
+}
