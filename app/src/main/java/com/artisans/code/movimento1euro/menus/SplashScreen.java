a0cc0f2b1e551c1f73989b1f1881629a4dea73f5
444Duarte <duartemr.pinto@gmail.com>
2016-11-15 16:26:48 +0000

    Firebase configurations added

--- app/src/main/java/com/artisans/code/movimento1euro/menus/SplashScreen.java
+++ app/src/main/java/com/artisans/code/movimento1euro/menus/SplashScreen.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
 
 /**
  * Created by duarte on 28-10-2016.
@@ -15,6 +18,9 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent;
+
+        String firebaseToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d("Firebase Token", firebaseToken);
 
         if(getSharedPreferences("userInfo",MODE_PRIVATE).getString("token", null) != null){
             intent = new Intent(this, MainMenu.class);
