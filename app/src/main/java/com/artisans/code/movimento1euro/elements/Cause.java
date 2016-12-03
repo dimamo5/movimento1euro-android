4e8cdbac6d855a6342748f624d194795ce0ed6b4
PedroCosta <up201303973@fe.up.pt>
2016-12-03 16:42:29 +0000

    Fixed: (Slight bug: list view is not displaying as it should. To be fixed.) -> Now displays (albeit, a bit distorted)

--- app/src/main/java/com/artisans/code/movimento1euro/elements/Cause.java
+++ app/src/main/java/com/artisans/code/movimento1euro/elements/Cause.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Antonio on 30-11-2016.
@@ -20,13 +21,22 @@ public class Cause {
     private String title;
     private String description;
     private String money;
+    private String votationMoney;
     private String votes;
     private ArrayList<Pair<URL, String>> documents = new ArrayList<Pair<URL, String>>();
     private ArrayList<Pair<URL, String>> videos = new ArrayList<Pair<URL, String>>();
     private boolean user_vote;
     private Association association;
 
-    private Cause () {
+    public Cause () {
+    }
+
+    public class Constants {
+        public static final String TITLE_COLUMN = "Title";
+        public static final String DESCRIPTION_COLUMN = "Description";
+        public static final String MONEY_COLUMN = "Money";
+        public static final String VOTES_COLUMN = "Votes";
+        public static final String VERBA_COLUMN = "Verba";
     }
 
     public static Cause parseVotingCause(JSONObject json) {
@@ -50,6 +60,26 @@ public class Cause {
         return cause;
     }
 
+    public static Cause parsePastCause(JSONObject json) {
+        Cause cause = new Cause();
+
+        try {
+            cause.id = json.getInt("id");
+            cause.title = json.getString("titulo");
+            cause.description = json.getString("descricao");
+            cause.money = json.getString("verba");
+            cause.votationMoney = json.getString("montante_disponivel");
+            cause.votes = json.getString("votos");
+            cause.association = new Association(json.getJSONObject("associacao"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.e("Cause exception", "Error parsing JSON");
+            Log.d("past", "Error: "  + e.getMessage());
+        }
+
+        return cause;
+    }
+
     private static ArrayList<Pair<URL, String>> parseArray(JSONArray jsonArray) throws JSONException, MalformedURLException {
         ArrayList<Pair<URL, String>> ret = new ArrayList<Pair<URL, String>>();
         for (int i = 0; i < jsonArray.length(); i++) {
@@ -57,6 +87,22 @@ public class Cause {
             ret.add(new Pair(new URL(obj.getString("url")), obj.getString("descricao")));
         }
         return ret;
+    }
+
+    public HashMap<String, String> toHashMap(){
+        HashMap<String, String> hashMap = new HashMap<String,String>();
+
+        hashMap.put(Constants.TITLE_COLUMN, title);
+        hashMap.put(Constants.DESCRIPTION_COLUMN, description);
+        hashMap.put(Constants.MONEY_COLUMN, votationMoney + " €");
+        hashMap.put(Constants.VOTES_COLUMN, votes+ " votes");
+        hashMap.put(Constants.VERBA_COLUMN, money + " €");
+
+        return hashMap;
+    }
+
+    public String toString(){
+        return "ID: " + id + ", Title: " + title + ", Description: " + description + ", Votes: " + votes + ", Money: " + money + ", Votation Money: " + votationMoney;
     }
 
     public int getId() {
@@ -74,6 +120,10 @@ public class Cause {
     public String getMoney() {
         return money;
     }
+
+    public String getVotationMoney() { return votationMoney; }
+
+    public void setVotationMoney(String votationMoney) { this.votationMoney = votationMoney; }
 
     public String getVotes() {
         return votes;
--- app/src/main/java/com/artisans/code/movimento1euro/fragments/ViewLastCausesFragment.java
+++ app/src/main/java/com/artisans/code/movimento1euro/fragments/ViewLastCausesFragment.java
@@ -7,6 +7,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Looper;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,6 +19,7 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.artisans.code.movimento1euro.R;
+import com.artisans.code.movimento1euro.elements.Cause;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
@@ -28,12 +30,11 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 
 import static android.content.Context.MODE_PRIVATE;
-import static com.artisans.code.movimento1euro.fragments.ViewLastCausesFragment.Constants.YEAR_COLUMN;
 
 
 /**
@@ -53,7 +54,7 @@ public class ViewLastCausesFragment extends Fragment {
     */
 
     public static final List<String> MONTHS = Arrays.asList("No Month", "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
-
+    public static final int START_YEAR = 2011;
     // TODO: Rename and change types of parameters
     /*
     private String mParam1;
@@ -63,11 +64,20 @@ public class ViewLastCausesFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
 
     ArrayList<String> yearsList = new ArrayList<String>();
+
+    // List of Cause objects. Cause objects are hashmaps
+    ArrayList<Cause> shownCauseslist = new ArrayList<Cause>();
     ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
-    ArrayList<HashMap<String, String>> fullList = new ArrayList<HashMap<String, String>>();
-    ArrayAdapter<String> spinnerAdapter;
+
+    HashMap<String, ArrayList<Cause>> allCausesByYear = new HashMap<String, ArrayList<Cause>>();
+
+    ArrayAdapter<String> spinnerAdapter; // Spinner year list adapter
+    Spinner spinner;
+    SimpleAdapter listAdapter; // Main causes list adapter
     ListView listView;
-    SimpleAdapter listAdapter;
+
+
+
 
     public ViewLastCausesFragment() {
         // Required empty public constructor
@@ -109,29 +119,44 @@ public class ViewLastCausesFragment extends Fragment {
 
     public class Constants {
         public static final String MONTH_COLUMN = "Month";
-        public static final String NAME_COLUMN = "Name";
-        public static final String MONEY_COLUMN = "Money";
         public static final String YEAR_COLUMN = "Year";
+        public static final String TITLE_COLUMN = "Title";
+        public static final String MONEY_COLUMN = "Money";
+        public static final String DESCRIPTION_COLUMN = "Description";
+        public static final String VOTES_COLUMN = "Votes";
+        public static final String VERBA_COLUMN = "Verba";
     }
 
 
     private class CausesTask extends AsyncTask<String, Void, JSONObject> {
 
+        String year;
+        boolean updateAfterRequest = false;
+
         public CausesTask() {
             super();
+        }
+
+        public CausesTask(boolean updateAfterRequest){
+            this.updateAfterRequest = updateAfterRequest;
         }
 
         @Override
         protected JSONObject doInBackground(String... parameters) {
 
+            //TODO: check if null
+            year = parameters[0];
+
+            // Preparation of variables for the request and response handling
             HttpResponse<String> response = null;
             JSONObject result = new JSONObject();
 
             SharedPreferences userDetails = getContext().getSharedPreferences("userInfo", MODE_PRIVATE);
             String token = userDetails.getString("token", "");
 
+            // API Request
             try {
-                response = Unirest.get(getResources().getString(R.string.api_server_url) + getResources().getString(R.string.winner_causes_path))
+                response = Unirest.get(getResources().getString(R.string.api_server_url) + getResources().getString(R.string.winner_causes_path)+ "?ano=" + year )
                         .header("accept", "application/json")
                         .header("content-type", "application/json")
                         .header("Authorization", token)
@@ -145,41 +170,53 @@ public class ViewLastCausesFragment extends Fragment {
                 if (response == null)
                     throw new Exception("Não foi possível carregar as causas passadas. Verifique a sua conexão.");
 
+                //TODO ask if result != success
                 JSONObject obj = new JSONObject(response.getBody());
-                JSONArray arr = obj.getJSONArray("causes");
+                // Get Yearly elections array from response
+                JSONArray yearlyElections = obj.getJSONArray("causes");
+                int totalElectionNr = yearlyElections.length();
 
-                yearsList.clear();
-                fullList.clear();
 
-                for (int i = 0; i < arr.length(); i++) {
-                    JSONObject o = arr.getJSONObject(i);
-                    String year = o.getString("date").substring(0, 4);
+                ArrayList<Cause> requestedCauses = new ArrayList<Cause>();
 
-                    if (!yearsList.contains(year)) {
-                        yearsList.add(year);
+                for (int electionNr = 0; electionNr < totalElectionNr; electionNr++) {
+
+                    // Get each election's winning causes and add them
+                    JSONObject election = yearlyElections.getJSONObject(electionNr);
+                    JSONArray winningCauses = election.getJSONArray("causas");
+                    int totalCausesNr = winningCauses.length();
+                    for(int causeNr = 0; causeNr < totalCausesNr; causeNr++) {
+
+                        JSONObject cause = winningCauses.getJSONObject(causeNr);
+
+                        //HashMap<String, String> tempCause = new HashMap<String, String>();
+                        //add titulo and montante to the cause object
+                        cause.put("titulo", election.getString("titulo"));
+                        cause.put("montante_disponivel", election.getString("montante_disponivel"));
+                        Cause tempCause = Cause.parsePastCause(cause);
+
+                        /*
+                        HashMap<String, String> tempCause = new HashMap<String, String>();
+
+                        tempCause.put(Constants.MONTH_COLUMN, election.getString("titulo"));
+                        tempCause.put(Constants.NAME_COLUMN, "Nome: " + cause.getString("nome"));
+                        tempCause.put(Constants.YEAR_COLUMN, election.getString("data_de_fim"));
+                        tempCause.put(Constants.MONEY_COLUMN, election.getString("montante_disponivel") + "€");
+                        tempCause.put(Constants.VERBA_COLUMN, "Verba: " + cause.getString("verba") + "€");
+                        tempCause.put(Constants.VOTES_COLUMN, "Votos: " + cause.getString("votos") + "€");
+                        */
+                        requestedCauses.add(tempCause);
                     }
-
-                    HashMap<String, String> temp = new HashMap<String, String>();
-
-                    int month =  Integer.parseInt(o.getString("month"));
-                    if(month > 12 || month < 0)
-                        month = 0;
-                    temp.put(Constants.MONTH_COLUMN, "Mês de " + MONTHS.get(month)); // -1 because index starts at 0
-
-                    temp.put(Constants.NAME_COLUMN, "Nome: " + o.getString("name"));
-                    temp.put(Constants.YEAR_COLUMN, year);
-                    temp.put(Constants.MONEY_COLUMN, i + "€");
-                    fullList.add(temp);
                 }
 
-                Collections.sort(yearsList);
+                allCausesByYear.put(year, requestedCauses);
 
-                //yearsList.add();
             } catch (JSONException e) {
-                // Log.d("causes", "JSONException: " + e.getMessage());
+                // Log.d("causes", "JSONException2: " + e.getMessage());
             } catch (Exception e) {
                 // Log.d("causes", "Exception: " + e.getMessage());
 
+                // Handle error
                 try {
                     Looper.prepare();
                     result.put("error", true);
@@ -193,9 +230,6 @@ public class ViewLastCausesFragment extends Fragment {
             // Map<String, String> headers = response.getHeaders();
             //InputStream rawBody = response.getRawBody();
 
-
-
-
             return result;
         }
 
@@ -206,7 +240,7 @@ public class ViewLastCausesFragment extends Fragment {
             try {
                 if(result != null) {  // RESULT != NULL MEANS THERE WAS AN ERROR
                     String message = result.getString("errorMessage");
-                    if (result.getBoolean("error") == true)
+                    if (result.getBoolean("error") == true && updateAfterRequest)
                         Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
                 }
             }catch(JSONException e){
@@ -215,26 +249,40 @@ public class ViewLastCausesFragment extends Fragment {
                 // Log.d("causes", b.getMessage());
             }
 
+            // Deprecated
+            if(updateAfterRequest) {
+                spinnerAdapter.notifyDataSetChanged();
+                Log.d("past", "I am on post execute updating: " + year);
+                updateFromSpinner(year);
+            }
 
-            spinnerAdapter.notifyDataSetChanged();
-            if(yearsList.size()!= 0)
-                updateFromSpinner(yearsList.get(0));
         }
     }
 
     public void updateFromSpinner(String year) {
-        list.clear();
-        for(HashMap<String, String> item : fullList) {
-            String y  = item.get(YEAR_COLUMN);
 
-            if(y.equals(year))
-                list.add(item);
+        Log.d("past", "Spinner: year = " + year);
+        if(allCausesByYear.get(year) != null) {
+
+            shownCauseslist = allCausesByYear.get(year);
+
+            Log.d("past", allCausesByYear.toString());
+            Log.d("past", shownCauseslist.toString());
+
+            causeList_to_hashmapList(shownCauseslist, list);
+
+
+            /*
+            Log.d("past", list.get(0).get(Constants.VOTES_COLUMN));
+            Log.d("past", list.get(0).get(Constants.MONEY_COLUMN));
+            Log.d("past", list.get(0).get(Constants.DESCRIPTION_COLUMN));
+            Log.d("past", list.get(0).get(Constants.TITLE_COLUMN));
+            Log.d("past", list.get(0).get(Constants.VERBA_COLUMN));
+            */
+        }else{
+            // Mostrar no ecrã que não há causas para este período(ano)
         }
-
-        // QUICK FIX FOR THE MONTH ORDER -> DEPENDS ON API ORDER
-        //TODO: Check if API will return ordered or manipulate lists to sort well
-        Collections.reverse(list);
-
+        Log.d("past", list.toString());
         listAdapter.notifyDataSetChanged();
     }
 
@@ -246,34 +294,22 @@ public class ViewLastCausesFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_previous_winners, container, false);
 
-        listView = (ListView) view.findViewById(R.id.causes_list);
-
-        new CausesTask().execute();
-        // Log.d("causes", "executed new CausesTask()");
-
-        /*
-        for (int i = 0; i < 50; i++) {
-            HashMap<String,String> temp=new HashMap<String, String>();
-            temp.put(Constants.MONTH_COLUMN, "Mes " + i);
-            temp.put(Constants.NAME_COLUMN, "Nome " + i);
-            temp.put(Constants.MONEY_COLUMN, i+"€");
-            list.add(temp);
+        // HARDCODED: GET THE YEARS WHERE THERE WERE CAUSES
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        if(currentYear >= START_YEAR) {
+            for (int year = currentYear; year >= START_YEAR; year--) {
+                yearsList.add(Integer.toString(year));
+            }
+        }else{
+            Log.d("past", Integer.toString(currentYear) + "is  <  than " + Integer.toString(START_YEAR));
+            yearsList.add(Integer.toString(START_YEAR));
         }
-        */
 
-        listAdapter = new SimpleAdapter(
-                this.getContext(),
-                list,
-                R.layout.item_previous_winner,
-                new String[]{Constants.MONTH_COLUMN, Constants.NAME_COLUMN, Constants.MONEY_COLUMN},
-                new int[]{R.id.last_causes_item_month, R.id.last_causes_item_name, R.id.last_causes_item_money}
-        );
-        listView.setAdapter(listAdapter);
 
-        Spinner spinner = (Spinner) getActivity().findViewById(R.id.spinner_nav);
+        // SPINNER - Create Spinner on ACTIVITY BAR + define adapter, functions and spinner items
+        spinner = (Spinner) getActivity().findViewById(R.id.spinner_nav);
         spinner.setVisibility(View.VISIBLE);
 
-        //TODO: Get existing years list
         spinnerAdapter = new ArrayAdapter<String>(this.getContext(),
                 R.layout.spinner_previous_winners_selected_year, yearsList);
         spinnerAdapter.setDropDownViewResource(R.layout.spinner_previous_winners_year);
@@ -283,7 +319,11 @@ public class ViewLastCausesFragment extends Fragment {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 //TODO spinner click
-                //Toast.makeText(getActivity(), yearsList.get(i) + " Clicked", Toast.LENGTH_SHORT).show();
+
+                // Old version: got values for the year when clicking. New version will have them already stored in cache
+                // on item selected only updates spinner with values
+                Log.d("past",  "I am on item selected updating: " + yearsList.get(i));
+               //new CausesTask().execute(yearsList.get(i), "true");
                 updateFromSpinner(yearsList.get(i));
             }
 
@@ -293,6 +333,26 @@ public class ViewLastCausesFragment extends Fragment {
             }
         });
 
+
+        // LIST - Create List adapter
+        listView = (ListView) view.findViewById(R.id.causes_list);
+
+        listAdapter = new SimpleAdapter(
+                this.getContext(),
+                list,
+                R.layout.item_previous_winner,
+                new String[]{Constants.VOTES_COLUMN, Constants.MONEY_COLUMN, Constants.DESCRIPTION_COLUMN, Constants.TITLE_COLUMN, Constants.VERBA_COLUMN},
+                new int[]{R.id.last_causes_item_votes, R.id.last_causes_item_money, R.id.last_causes_item_description, R.id.last_causes_item_title, R.id.last_causes_item_verba}
+        );
+        listView.setAdapter(listAdapter);
+
+        // API request -> with the first year in the list, which should currently be selected
+        // Maybe execute all already, to store them
+        new CausesTask(true).execute(yearsList.get(0));
+        Log.d("past", "Year to be executed: " + yearsList.get(0));
+        for(int i = 1; i< yearsList.size(); i++) {
+            new CausesTask(false).execute(yearsList.get(i));
+        }
         return view;
     }
 
@@ -339,4 +399,16 @@ public class ViewLastCausesFragment extends Fragment {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+
+    public ArrayList<HashMap<String, String>> causeList_to_hashmapList(ArrayList<Cause> causeList, ArrayList<HashMap<String,String>> hashmapList){
+        hashmapList.clear();
+
+        if(causeList != null)
+        for(int i = 0; i < causeList.size(); i++){
+            hashmapList.add(causeList.get(i).toHashMap());
+        }
+        return hashmapList;
+    }
+
 }
