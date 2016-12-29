83392b0228328a1507843cd8e4a9fe8ca3036b49
António Ramadas <antonio@Antonios-MBP.home>
2016-12-29 17:22:45 +0000

    API integration

--- app/src/main/java/com/artisans/code/movimento1euro/menus/MainMenu.java
+++ app/src/main/java/com/artisans/code/movimento1euro/menus/MainMenu.java
@@ -1,9 +1,12 @@
 package com.artisans.code.movimento1euro.menus;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -18,6 +21,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.artisans.code.movimento1euro.R;
 import com.artisans.code.movimento1euro.fragments.CauseListFragment;
@@ -31,14 +35,16 @@ import java.util.concurrent.TimeUnit;
 import com.artisans.code.movimento1euro.fragments.VotingCausesFragment;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainMenu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
         CauseListFragment.OnFragmentInteractionListener {
 
     public static final String TAG = MainMenu.class.getSimpleName();
-    //TODO get the official number for the warning
-    private static final int WARNING_DAYS_BEFORE_EXPIRATION = 5;
 
     protected String NEWS_URL;
     protected String ABOUT_US_URL;
@@ -219,16 +225,31 @@ public class MainMenu extends AppCompatActivity
     protected void onStart() {
         super.onStart();
 
-        refreshInfo();
-
+        fillUserDetails();
     }
 
-    private void refreshInfo() {
+    private void fillUserDetails() {
+
+        // if necessary, it displays to the user a dialog warning about the proximity of the due date
+        new AlertTask(this).execute();
+
+        // fill user info (name and expiration date)
+        SharedPreferences preferences = getSharedPreferences("userInfo",MODE_PRIVATE);
+        String username = preferences.getString("username", "");
+
+        Date expDate = new Date(preferences.getString("expDate",""));
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-mm-yyyy");
+        String expDateStr = sdf.format(expDate);
+
+        this.username.setText(username);
+        this.expDate.setText(expDateStr);
+    }
+
+    private void refreshInfo(int delta, String title, String msg) {
 
         //refreshInfoRequest();
 
         SharedPreferences preferences = getSharedPreferences("userInfo",MODE_PRIVATE);
-        String username = preferences.getString("username", "");
 
         Date expDate = new Date(preferences.getString("expDate",""));
         SimpleDateFormat sdf = new SimpleDateFormat("dd-mm-yyyy");
@@ -238,10 +259,12 @@ public class MainMenu extends AppCompatActivity
 
         long diffDays = TimeUnit.DAYS.convert(expDate.getTime() - currentDate.getTime(), TimeUnit.MILLISECONDS);
 
-        if (diffDays > WARNING_DAYS_BEFORE_EXPIRATION) {
+        msg.replace("@proxPagamento", Long.toString(diffDays) + " " + (diffDays == 1 ? "dia" : "dias"));
+
+        if (diffDays > delta) {
             new AlertDialog.Builder(this)
-                    .setTitle("Aviso")
-                    .setMessage("Faltam " + diffDays + " dias para acabar o pagamento!")
+                    .setTitle(title)
+                    .setMessage(msg)
                     .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int which) {
                             // continue with delete
@@ -251,12 +274,112 @@ public class MainMenu extends AppCompatActivity
                     .show();
         }
 
-        this.username.setText(username);
         this.expDate.setText(expDateStr);
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
         
+    }
+
+    private class AlertTask extends AsyncTask<String, Void, JSONObject> {
+
+        AppCompatActivity activity;
+
+        public AlertTask() {
+            super();
+        }
+
+        public AlertTask(AppCompatActivity activity) {
+            this.activity = activity;
+        }
+
+        @Override
+        protected JSONObject doInBackground(String... parameters) {
+
+            // Preparation of variables for the request and response handling
+            HttpResponse<String> response = null;
+            JSONObject result = new JSONObject();
+            String token = "";
+
+            try {
+                SharedPreferences userDetails = getSharedPreferences("userInfo", MODE_PRIVATE);
+                token = userDetails.getString("token", "");
+            }catch(Exception e){
+
+                //To prevent conflicts between async tasks, if user clicks various times on the menu item
+                return result;
+
+            }
+            // API Request
+            try {
+                response = Unirest.get(getResources().getString(R.string.api_server_url) + getResources().getString(R.string.days_to_warn_path))
+                        .header("accept", "application/json")
+                        .header("content-type", "application/json")
+                        .header("Authorization", token)
+                        .asString();
+            } catch (UnirestException e) {
+
+            }
+
+            try {
+                if (response == null) {
+                    ConnectivityManager cm =
+                            (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+
+                    NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+                    boolean isConnected = activeNetwork != null &&
+                            activeNetwork.isConnectedOrConnecting();
+
+                    String connectionError = getResources().getString(R.string.user_connection_error);
+                    String requestError = getResources().getString(R.string.days_to_warn_request_error);
+
+                    String error = isConnected ? requestError : connectionError;
+
+                    throw new Exception(error);
+                }
+
+                JSONObject obj = new JSONObject(response.getBody());
+                if (!obj.getString("result").equals(getResources().getString(R.string.api_success_response)))
+                    throw new Exception(getResources().getString(R.string.user_loading_authetication_error));
+
+                result.put("daysToWarn", obj.getInt("daysToWarn"));
+                result.put("title", obj.getString("alertTitle"));
+                result.put("msg", obj.getString("alertMsg"));
+
+            } catch (JSONException e) {
+
+            } catch (Exception e) {
+                // Handle error
+                try {
+                    result.put("error", true);
+                    result.put("errorMessage", e.getMessage());
+                }catch(Exception b){
+
+                }
+            }
+
+            return result;
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(JSONObject result) {
+
+            try {
+                if (result != null) {  // RESULT != NULL MEANS THERE WAS AN ERROR
+                    String message = result.getString("errorMessage");
+
+                    //only shows toast for the request which was to update screen, in case several requests are made
+                    if (result.getBoolean("error")) {
+                        Toast.makeText(activity, message, Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    refreshInfo(result.getInt("daysToWarn"), result.getString("title"), result.getString("msg"));
+                }
+            } catch (JSONException e) {
+                // Log.d("causes", e.getMessage());
+            }
+        }
     }
 }
